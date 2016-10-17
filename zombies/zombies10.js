97f6bdbe9b6f0348188ed3dc7c855607feb9dc52
Johan Jordaan <johan@inmydiary.com>
2016-10-18 08:56:47 +1100

    Added zombies 10's tests

--- zombies/zombies10.js
+++ zombies/zombies10.js
@@ -1,29 +1,29 @@
 var _ = require('underscore');
 var nox = require('../');
 
-var walkerDef = {
+var walkerSpec = {
    mod: 1.5,
    hp: nox.rnd.int({min:10,max:20}),
    xp: nox.method({method: (o)=>{return(o.hp*o.mod);}}),
    type: 'walker'
 };
-var walkerTemplate = nox.createTemplate("walker_",walkerDef);
+var walkerTemplate = nox.createTemplate("walker_",walkerSpec);
 
-var romeoDef = {
+var romeoSpec = {
    hp: nox.rnd.int({ min:30,max:50 }),
    mod: 2,
    type: 'romeo'
 };
-var romeoTemplate = nox.extendTemplate(walkerTemplate,"romeo_",romeoDef);
+var romeoTemplate = nox.extendTemplate(walkerTemplate,"romeo_",romeoSpec);
 
-var foxtrotDef = {
+var foxtrotSpec = {
   hp: nox.rnd.int({min:80,max:150}),
   mod: 3,
   type: 'foxtrot'
 };
-var foxtrotTemplate = nox.extendTemplate(walkerTemplate,"foxtrot_",foxtrotDef);
+var foxtrotTemplate = nox.extendTemplate(walkerTemplate,"foxtrot_",foxtrotSpec);
 
-var zombieSwarmDef = {
+var zombieSwarmSpec = {
    zombies: nox.select({
       count: nox.rnd({min:5,max:15}),
       values:[
@@ -33,11 +33,24 @@ var zombieSwarmDef = {
       ]}),
 };
 
-nox.createTemplate("zombieSwarm",zombieSwarmDef);
-var zombieSwarm = nox.constructTemplate("zombieSwarm");
+var zombieSwarmTemplate = nox.createTemplate("ZombieSwarm",zombieSwarmSpec);
+var zombieSwarm = nox.constructTemplate("ZombieSwarm");
 
 _.each(zombieSwarm.zombies,(zombie)=>{
    console.log(nox.deNox(zombie));
 });
 
-module.exports = zombieSwarm;
+module.exports = {
+   walkerSpec: walkerSpec,
+   romeoSpec: romeoSpec,
+   foxtrotSpec: foxtrotSpec,
+
+   walkerTemplate: walkerTemplate,
+   romeoTemplate: romeoTemplate,
+   foxtrotTemplate: foxtrotTemplate,
+
+   zombieSwarmSpec: zombieSwarmSpec,
+   zombieSwarmTemplate: zombieSwarmTemplate,
+
+   zombieSwarm: zombieSwarm,
+};
